c8c9ec00533a4987bc46c551629cc08acacad6f5
Christian Malaitai <christian@asdah.school.nz>
2020-11-26 11:25:06 +1300

    Updated

--- Level 3/Gio/waka_gio/Final.py
+++ Level 3/Gio/waka_gio/Final.py
@@ -1,13 +1,16 @@
+# Created by: Giovaani
+
 import tkinter as tk
 from os import walk
 
+# Displays the clubs for the Waka competition
 def display_clubs(clubs):
     display = tk.Toplevel(root)
     display.title("All Club Points")
     display.iconbitmap("WakaAma.ico")
     
     #Turn dict into list
-    results = [ [k,v] for k, v in clubs.items()]
+    results = [[k,v] for k, v in clubs.items()]
     
     # Order list
     results.sort(key = lambda x: x[1])
@@ -18,16 +21,14 @@ def display_clubs(clubs):
     # Adds each waka to the Lsitbox "results_listbox"
     for x in results:
         
-        
         if(x[[0][0]] == ""):
             continue
             
         adding_student = "{}".format(x)
         results_listbox.insert(0, adding_student)
-        
+
     results_listbox.grid(row = 0, column = 0)
     print(results)
-    
     
     results = tk.Listbox(display)
     for x in races:
@@ -37,7 +38,7 @@ def display_clubs(clubs):
     results.grid(row = 0, column = 0)
 
 
-
+# Calculates the scores for each Waka team
 def calculate_scores(current_year, final_races):
     clubs = {}
     
@@ -91,7 +92,8 @@ def calculate_scores(current_year, final_races):
             # Converting into dictionary list
     print(clubs)
     display_clubs(clubs)
-        
+
+# Displays the files for the user to choose from
 def display_files():
     races = []
     current_year = year_chosen.get()
@@ -123,24 +125,29 @@ BG_COLOUR="#C933FF"# Purple
 FG_COLOUR="#5FFF33"# Green
 root = tk.Tk()
 
-
+# Providing an icon and title for the GUI
 root.title("Waka Ama Competition")
 root.configure(bg=BG_COLOUR)
 root.iconbitmap("WakaAma.ico")
 root.geometry("400x150")
 
+# Title label for user to see
 title = tk.Label(root, text="The Waka Ama Spring National Competition", font = ("Ariel", [14]), bg=BG_COLOUR, fg=FG_COLOUR)
 title.grid(row = 0, column = 0)
 
+# Message label
 title = tk.Label(root, text = "Champions of 2017 & 2018", font = ("Ariel", [14]), bg=BG_COLOUR, fg=FG_COLOUR)
 title.grid(row = 1, column = 0)
 
+# Default "Select Year" for OptionMenu widget
 year_chosen = tk.StringVar()
 year_chosen.set("Select Year")
 
+# OptionMenu widget to choose folder
 year_options = tk.OptionMenu(root, year_chosen, *years)
 year_options.grid(row = 3, column = 0)
 
+# Button for user to display folders (2017, 2018)
 button = tk.Button(root, text = "Display files", command = display_files)
 button.grid(row = 4, column = 0)
 
